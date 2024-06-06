1a21fd3ed451242dd896fb4d9d44aef80171d6b1
NguyenTuanCanh <tuancanhnguyen706@gmail.com>
2024-06-06 08:09:22 +0700

    haha

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ const folderC = path.resolve(__dirname, '../tracks');
 // Lệnh bạn muốn chạy trong thư mục C
 const command = 'go run cmd/main.go start'; // Dùng 'dir' cho Windows, 'ls' cho Unix
 
-exec(command, { cwd: folderC }, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Lỗi khi chạy lệnh: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Lỗi stderr: ${stderr}`);
-        return;
-    }
-    console.log(`Kết quả: ${stdout}`);
-});
+// Chạy lệnh trong thư mục C
+setInterval(() => {
+    exec(command, { cwd: folderC }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Lỗi khi chạy lệnh: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Lỗi stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Kết quả: ${stdout}`);
+    });
+}, 600000);
